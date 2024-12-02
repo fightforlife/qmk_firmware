5678bcaf65665158ecc0017dc02600a6b87090f8
Frederik <5511687+fightforlife@users.noreply.github.com>
2024-12-02 21:32:50 +0100

    a little stack optimisation

--- keyboards/womier/k66v1/rules.mk
+++ keyboards/womier/k66v1/rules.mk
@@ -7,8 +7,8 @@ SRC += custom_drivers/rand.c
 # reduce stack size
 USE_EXCEPTIONS_STACKSIZE = 0x120
 # 0x140 OK 0x100
-USE_PROCESS_STACKSIZE = 0x1A0
+USE_PROCESS_STACKSIZE = 0x1a0
 # 0x190 OK 0z180
 TOP_SYMBOLS = yes
 
-#EXTRAFLAGS += -flto -O3 #more RAM with O3
+EXTRAFLAGS += -flto -Os#-O3 #more RAM with O3
