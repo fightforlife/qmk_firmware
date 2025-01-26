da9bc9b82971f0a5ff551f5c30f51bd9d5c4a732
fightforlife <5511687+fightforlife@users.noreply.github.com>
2025-01-26 14:08:50 +0100

    ram reduction, after upstream changes

--- keyboards/womier/k66v1/rules.mk
+++ keyboards/womier/k66v1/rules.mk
@@ -5,7 +5,7 @@ SRC += custom_drivers/no_idle_thread.c
 SRC += custom_drivers/rand.c
 
 # reduce stack size
-USE_EXCEPTIONS_STACKSIZE = 0x120
+USE_EXCEPTIONS_STACKSIZE = 0x118
 # 0x140 OK 0x100
 USE_PROCESS_STACKSIZE = 0x1a0
 # 0x190 OK 0z180
